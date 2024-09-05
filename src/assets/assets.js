5d6b7fd6103fa7d53a4d22b509887662cfb8a6ac
karan <114896722+karan4533@users.noreply.github.com>
2024-09-06 00:03:20 +0530

    commit

---
+++ src/assets/assets.js
@@ -0,0 +1,80 @@
+import basket_icon from './basket_icon.png'
+import logo from './logo.png'
+import header_img from './header_img.png'
+import search_icon from './search_ico.png'
+import menu_1 from './menu_1.png'
+import menu_2 from './menu_2.png'
+import menu_3 from './menu_3.png'
+import menu_4 from './menu_4.png'
+import menu_5 from './menu_5.png'
+import menu_6 from './menu_6.png'
+import menu_7 from './menu_7.png'
+import menu_8 from './menu_8.png'
+
+import meat_1 from './meat_1.png'
+import meat_2 from './meat_2.png'
+import meat_3 from './meat_3.png'
+import meat_4 from './meat_4.png'
+import meat_5 from './meat_5.png'
+import meat_6 from './meat_6.png'
+import meat_7 from './meat_7.png'
+import meat_8 from './meat_8.png'
+import meat_9 from './meat_9.png'
+import meat_10 from './meat_10.png'
+import meat_11 from './meat_11.png'
+import meat_12 from './meat_12.png'
+import meat_13 from './meat_13.png'
+import meat_14 from './meat_14.png'
+import meat_15 from './meat_15.png'
+import meat_16 from './meat_16.png'
+import meat_17 from './meat_17.png'
+import meat_18 from './meat_18.png'
+import meat_19 from './meat_19.png'
+import meat_20 from './meat_20.png'
+import meat_21 from './meat_21.png'
+import meat_22 from './meat_22.png'
+import meat_23 from './meat_23.png'
+import meat_24 from './meat_24.png'
+import meat_25 from './meat_25.png'
+import meat_26 from './meat_26.png'
+import meat_27 from './meat_27.png'
+import meat_28 from './meat_28.png'
+import meat_29 from './meat_29.png'
+import meat_30 from './meat_30.png'
+import meat_31 from './meat_31.png'
+import meat_32 from './meat_32.png'
+
+import add_icon_white from './add_icon_white.png'
+import add_icon_green from './add_icon_green.png'
+import remove_icon_red from './remove_icon_red.png'
+import app_store from './app_store.png'
+import play_store from './play_store.png'
+import linkedin_icon from './linkedin_icon.png'
+import facebook_icon from './facebook_icon.png'
+import twitter_icon from './twitter_icon.png'
+import cross_icon from './cross_icon.png'
+import selector_icon from './selector_icon.png'
+import rating_starts from './rating_starts.png'
+import profile_icon from './profile_icon.png'
+import bag_icon from './bag_icon.png'
+import logout_icon from './logout_icon.png'
+import parcel_icon from './parcel_icon.png'
+
+export const assets = {
+    logo,
+    basket_icon,
+    header_img,
+    search_icon,
+    rating_starts,
+    add_icon_green,
+    add_icon_white,
+    remove_icon_red,
+    app_store,
+    play_store,
+    linkedin_icon,
+    facebook_icon,
+    twitter_icon,
+    cross_icon,
+    selector_icon,
+    profile_icon,
+};
